334470ae4298d232eb2f165c3a9ceb4b2ee2533f
Mitchell Grenier <mitchell@confurious.io>
2024-08-25 19:56:05 -0700

    Fix global harness

---
+++ testing/integration.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Build all of the Plaid workspace
+cargo build --all
+
+# Run Plaid and wait for it to finish starting
+RUST_LOG=debug cargo run --bin=plaid -- --config plaid/resources/plaid.toml --secrets plaid/resources/secrets.example.json &
+PLAID_PID=$!
+sleep 10
+
+# Set the variables the test harnesses will need
+export PLAID_LOCATION="localhost:4554"
+
+# Loop through all test modules in the test_modules directory
+for module in test_modules/*; do
+  # If the module is a directory
+  if [ -d "$module" ]; then
+    # If the module has a harness.sh file
+    if [ -f "$module/harness/harness.sh" ]; then
+      # Run the harness.sh file
+      bash $module/harness/harness.sh
+      # If the harness.sh file returns an error
+      if [ $? -ne 0 ]; then
+       echo "Integration test failed for module $module"
+        # Kill the Plaid process
+        kill $PLAID_PID
+        # Exit with an error
+        exit 1
+      fi
+    fi
+  fi
+done
+
+# Kill the Plaid process
+kill $PLAID_PID
