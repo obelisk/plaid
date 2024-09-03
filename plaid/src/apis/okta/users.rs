58da71332e8f96a6e7c0c8e309b6e4955ea5bada
Michele Minelli <michele.minelli@smartcontract.com>
2024-09-03 16:34:17 +0200

    Support authentication as Okta app

--- plaid/src/apis/okta/users.rs
+++ plaid/src/apis/okta/users.rs
@@ -1,14 +1,14 @@
 use crate::apis::{ApiError, okta::OktaError};
 
-use super::Okta;
+use super::{Okta, OktaOperation};
 
 impl Okta {
     pub async fn get_user_data(&self, query: &str, _: &str) -> Result<String, ApiError> {
         let res = self.client.get(format!("https://{}/api/v1/users/{}", &self.config.domain, query))
-            .header("Authorization", format!("SSWS {}", &self.config.token))
+            .header("Authorization", self.get_authorization_header(&OktaOperation::GetUserInfo).await.map_err(|e| ApiError::OktaError(e))?)
             .header("Content-Type", "application/json")
             .header("Accept", "application/json");
-    
+            
         let response = res.send().await.map_err(|e| ApiError::NetworkError(e))?;
         let data = response.bytes().await.map_err(|e| ApiError::NetworkError(e))?;
 
