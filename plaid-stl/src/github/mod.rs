2fb02dffcf4fe6402efdf66ca1ec1b91e26971d7
Michele Minelli <michele.minelli@smartcontract.com>
2024-11-07 15:23:57 -0800

    Add support for triggering GHA via repository_dispatch (#50)

--- plaid-stl/src/github/mod.rs
+++ plaid-stl/src/github/mod.rs
@@ -124,6 +124,17 @@ pub struct GithubFileContent {
 
 // END OF Structs used for deserializing GH's response when searching for code
 
+/// Parameters sent to the runtime when triggering a GHA via repository_dispatch.
+/// See https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-a-repository-dispatch-event for more details
+#[derive(Serialize, Deserialize)]
+pub struct RepositoryDispatchParams<T> {
+    pub owner: String,
+    pub repo: String,
+    pub event_type: String,
+    /// This is arbitrary content. We just need to be able to (de)serialize it.
+    pub client_payload: T,
+}
+
 impl FileSearchResultItem {
     /// Retrieve the content of the search result
     pub fn retrieve_raw_content(&self) -> Result<String, PlaidFunctionError> {
@@ -1430,4 +1441,38 @@ pub fn filter_search_results(
         filtered_results.push(result);
     }
     filtered_results
+}
+
+/// Trigger a GHA workflow via repository_dispatch.
+/// For more details, see
+/// * https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-a-repository-dispatch-event
+/// * https://docs.github.com/en/actions/writing-workflows/choosing-when-your-workflow-runs/events-that-trigger-workflows#repository_dispatch
+pub fn trigger_repo_dispatch<T>(
+    owner: &str,
+    repo: &str,
+    event_type: &str,
+    client_payload: T
+) -> Result<(), PlaidFunctionError> where T: Serialize + Deserialize<'static> {
+    extern "C" {
+        new_host_function!(github, trigger_repo_dispatch);
+    }
+
+    let params = RepositoryDispatchParams::<T> {
+        owner: owner.to_string(),
+        repo: repo.to_string(),
+        event_type: event_type.to_string(),
+        client_payload
+    };
+
+    let params = serde_json::to_string(&params).unwrap();
+    let res =
+        unsafe { github_trigger_repo_dispatch(params.as_bytes().as_ptr(), params.as_bytes().len()) };
+
+    // There was an error with the Plaid system. Maybe the API is not
+    // configured.
+    if res < 0 {
+        return Err(res.into());
+    }
+
+    Ok(())
 }
--- plaid/src/apis/github/mod.rs
+++ plaid/src/apis/github/mod.rs
@@ -1,3 +1,4 @@
+mod actions;
 mod code;
 mod copilot;
 mod environments;
