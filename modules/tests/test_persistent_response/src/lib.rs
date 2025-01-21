4e8af5488bac35afafddc5af7b2b3b834402cdbb
Michele Minelli <michele.minelli@smartcontract.com>
2025-01-21 19:09:27 +0100

    Split get_accessory_data and implement universal accessory data.

--- modules/tests/test_persistent_response/src/lib.rs
+++ modules/tests/test_persistent_response/src/lib.rs
@@ -5,7 +5,7 @@ entrypoint_with_source_and_response!();
 fn main(log: String, _source: LogSource) -> Result<Option<String>, i32> {
     plaid::print_debug_string(&format!("Testing Persistent Response With Log: [{log}]"));
 
-    let query_data = plaid::get_accessory_data_by_name("querydata").unwrap();
+    let query_data = plaid::get_query_params("querydata").unwrap();
 
     let webpage = include_str!("../resources/index.html")
         .to_string()
