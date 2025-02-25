e97e0e3eb87b8a59f0a44116994ca9c71dd1c4ec
Mitchell Grenier <mitchell@confurious.io>
2025-02-24 18:19:52 -0800

    Add extra test for API which is denied at runtime code level

--- modules/tests/test_testmode/harness/harness.sh
+++ modules/tests/test_testmode/harness/harness.sh
@@ -6,8 +6,7 @@ set -e
 URL="testmode"
 
 # Call the webhook
-OUTPUT=$(curl -XPOST -d 'crash' http://$PLAID_LOCATION/webhook/$URL)
-OUTPUT=$(curl -XPOST -d 'memory_pressure' http://$PLAID_LOCATION/webhook/$URL)
+OUTPUT=$(curl -XPOST -d 'unneeded' http://$PLAID_LOCATION/webhook/$URL)
 
 # There is no defined success critieria here but it's important
 # to keep track of what Plaid does as the compiler's optimizations
--- modules/tests/test_testmode/src/lib.rs
+++ modules/tests/test_testmode/src/lib.rs
@@ -7,6 +7,7 @@ entrypoint_with_source!();
 fn main(log: String, _: LogSource) -> Result<(), i32> {
     plaid::print_debug_string(&format!("Testing testmode With Log: [{log}]"));
 
+    // This should work because the configuration allows this particular request
     match network::make_named_request("testmode_allow", "OK", HashMap::new()) {
         Ok(_) => {
             plaid::print_debug_string("Testmode allowed request succeeded");
@@ -17,6 +18,7 @@ fn main(log: String, _: LogSource) -> Result<(), i32> {
         }
     }
 
+    // This should not work because the configuration denies this particular request
     let _: Result<(), ()> = match network::make_named_request("testmode_deny", "OK", HashMap::new())
     {
         Err(PlaidFunctionError::TestMode) => Ok(()),
@@ -28,5 +30,18 @@ fn main(log: String, _: LogSource) -> Result<(), i32> {
         }
     };
 
+    // This should fail because simple_json_post_request is not allowed in test mode
+    match network::simple_json_post_request("https://captive.apple.com", "{}", None) {
+        Err(PlaidFunctionError::TestMode) => {
+            plaid::print_debug_string("Testmode denied request succeeded");
+        }
+        Err(e) => {
+            plaid::print_debug_string(&format!("Testmode denied request failed: [{e}]"));
+            panic!("Testmode denied request failed");
+        }
+        Ok(_) => {
+            panic!("Testmode request that should have failed succeeded");
+        }
+    }
     Ok(())
 }
