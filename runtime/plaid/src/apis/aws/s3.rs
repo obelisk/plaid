e0ee29a19032b3eccbe1905a3b0c0ddfed4e8740
Will Spencer <william.spencer@smartcontract.com>
2025-05-28 14:28:08 -0400

    Comments

--- runtime/plaid/src/apis/aws/s3.rs
+++ runtime/plaid/src/apis/aws/s3.rs
@@ -9,6 +9,7 @@ use crate::{apis::ApiError, get_aws_sdk_config, loader::PlaidModule, AwsAuthenti
 use aws_sdk_s3::operation::get_object::GetObjectError;
 use aws_sdk_s3::operation::put_object::PutObjectError;
 
+/// Errors that may occur while interacting with S3.
 #[derive(Debug)]
 pub enum S3Errors {
     S3PutObjectError(SdkError<PutObjectError>),
@@ -17,9 +18,12 @@ pub enum S3Errors {
     PresignError(aws_sdk_s3::presigning::PresigningConfigError),
 }
 
+/// Permissions for an S3 bucket.
 #[derive(Deserialize, PartialEq)]
 enum BucketPermission {
+    /// Read-only access.
     Read,
+    /// Write access.
     Write,
 }
 
@@ -29,61 +33,66 @@ impl Default for BucketPermission {
     }
 }
 
+/// Configuration for a single S3 bucket, including permissions and the allowed rule.
 #[derive(Deserialize)]
 struct BucketConfiguration {
+    /// Access permission (Read or Write). Defaults to `Read` if a value isn't provided
     #[serde(default)]
     permission: BucketPermission,
+    /// Rule name that is allowed to access this bucket.
     rule: String,
 }
 
-/// Defines configuration for the KMS API
+/// Configuration for initializing the S3 API wrapper.
 #[derive(Deserialize)]
 pub struct S3Config {
-    /// Specifies the authentication method for accessing the KMS API.
-    ///
-    /// This can either be:
-    /// - `IAM`: Uses the IAM role assigned to the instance or environment.
-    /// - `ApiKey`: Uses explicit credentials, including an access key ID, secret access key, and region.
+    /// Authentication method used to access S3.
     authentication: AwsAuthentication,
-    /// Configured bucket - maps an S3 bucket ID to a list of rules that are allowed to use it
+    /// Maps S3 bucket names to their associated configuration rules.
     bucket_configuration: HashMap<String, Vec<BucketConfiguration>>,
 }
 
-/// Represents the KMS API that handles all requests to KMS
+/// S3 wrapper for interacting with the AWS S3 service based on bucket/rule configuration.
 pub struct S3 {
-    /// The underlying KMS client used to interact with the KMS API.
+    /// AWS SDK S3 client.
     client: Client,
-    /// Configured bucket - maps an S3 bucket ID to a list of rules that are allowed to use it
+    /// Configured buckets and their associated access rules.
     bucket_configuration: HashMap<String, Vec<BucketConfiguration>>,
 }
 
+/// Request payload for uploading an object to S3.
 #[derive(Deserialize)]
 struct PutObjectRequest {
-    /// The bucket name to which the `PUT` action was initiated.
+    /// Target bucket ID.
     bucket_id: String,
-    /// Object data.
+    /// Object data as a byte vector.
     object: Vec<u8>,
-    /// Object key for which the `PUT` action was initiated.
+    /// Key to store the object under.
     object_key: String,
 }
 
+/// Request payload for retrieving an object from S3.
 #[derive(Deserialize)]
 struct GetObjectRequest {
-    /// The bucket name containing the object.
+    /// Bucket ID to fetch the object from.
     bucket_id: String,
-    /// Key of the object to get.
+    /// Key of the object to fetch.
     object_key: String,
+    /// Optional duration in seconds for generating a presigned URL.
     expires_in: Option<u64>,
 }
 
+/// Response returned from `get_object`.
 #[derive(Serialize)]
 enum GetObjectReponse {
+    /// Raw object data.
     Object(Vec<u8>),
+    /// Presigned URL to access the object.
     PresignedUri(String),
 }
 
 impl S3 {
-    /// Creates a new instance of `S3`
+    /// Creates a new `S3` instance using the provided configuration.
     pub async fn new(config: S3Config) -> Self {
         let sdk_config = get_aws_sdk_config(config.authentication).await;
         let client = aws_sdk_s3::Client::new(&sdk_config);
@@ -94,16 +103,15 @@ impl S3 {
         }
     }
 
+    /// Handles a request to retrieve an object from S3 or generate a presigned URL.
     pub async fn get_object(
         &self,
         params: &str,
         module: Arc<PlaidModule>,
     ) -> Result<String, ApiError> {
-        // Parse the information needed to make the request
         let request =
             serde_json::from_str::<GetObjectRequest>(params).map_err(|_| ApiError::BadRequest)?;
 
-        // Check that caller is allowed to read from this bucket
         self.fetch_bucket_configuration(module.clone(), &request.bucket_id)?;
 
         let request_builder = self
@@ -114,25 +122,25 @@ impl S3 {
 
         let response = if let Some(expiration) = request.expires_in {
             let presigned = PresigningConfig::expires_in(Duration::from_secs(expiration))
-                .map_err(|e| S3Errors::PresignError(e))?;
+                .map_err(S3Errors::PresignError)?;
 
             let response = request_builder
                 .presigned(presigned)
                 .await
-                .map_err(|e| S3Errors::S3GetObjectError(e))?;
+                .map_err(S3Errors::S3GetObjectError)?;
 
             GetObjectReponse::PresignedUri(response.uri().to_string())
         } else {
             let response = request_builder
                 .send()
                 .await
-                .map_err(|e| S3Errors::S3GetObjectError(e))?;
+                .map_err(S3Errors::S3GetObjectError)?;
 
             let object_bytes = response
                 .body
                 .collect()
                 .await
-                .map_err(|e| S3Errors::BytesStreamError(e))?
+                .map_err(S3Errors::BytesStreamError)?
                 .into_bytes();
 
             GetObjectReponse::Object(object_bytes.to_vec())
@@ -143,6 +151,7 @@ impl S3 {
         Ok(serialized)
     }
 
+    /// Handles a request to upload an object to S3.
     pub async fn put_object(
         &self,
         params: &str,
@@ -166,11 +175,12 @@ impl S3 {
             .key(request.object_key)
             .send()
             .await
-            .map_err(|e| S3Errors::S3PutObjectError(e))?;
+            .map_err(S3Errors::S3PutObjectError)?;
 
         Ok(String::new())
     }
 
+    /// Verifies that the module is authorized to access the specified bucket
     fn fetch_bucket_configuration<T: Display>(
         &self,
         module: T,
@@ -188,8 +198,8 @@ impl S3 {
                 }
             }
             None => {
-                error!("{module} tried to use a S3 bucket that is not configured: {bucket}",);
-                return Err(ApiError::BadRequest);
+                error!("{module} tried to use a S3 bucket that is not configured: {bucket}");
+                Err(ApiError::BadRequest)
             }
         }
     }
