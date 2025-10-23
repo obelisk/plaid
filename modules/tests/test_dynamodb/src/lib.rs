98e61c2be02cd3bbaa217bd8dec9e990b59060a4
Harry Anderson <harry.anderson@smartcontract.com>
2025-10-23 16:40:35 +1100

    feat: add more to test_dynamodb

--- modules/tests/test_dynamodb/src/lib.rs
+++ modules/tests/test_dynamodb/src/lib.rs
@@ -1,11 +1,11 @@
+use serde_json::{json, to_value, Value};
 use std::collections::HashMap;
 
 use base64::{prelude::BASE64_STANDARD, Engine};
 use plaid_stl::{
-    aws::dynamodb::{self, PutItemInput},
+    aws::dynamodb::{self, DeleteItemInput, PutItemInput, QueryInput},
     entrypoint_with_source,
     messages::LogSource,
-    network::make_named_request,
     plaid,
 };
 
@@ -14,7 +14,7 @@ entrypoint_with_source!();
 fn main(log: String, _: LogSource) -> Result<(), i32> {
     plaid::print_debug_string(&format!("Testing dynamodb With Log: [{log}]"));
     let table_name = String::from("local_test");
-    // PutItem with all attribute types
+    // PutItem (with all attribute types)
     // NOTE:: The json object represents an item
     // in a dynamodb table, each key is an attribute (column)
     // the primary + secondary keys must also be contained
@@ -50,12 +50,66 @@ fn main(log: String, _: LogSource) -> Result<(), i32> {
     };
 
     match dynamodb::put_item(input) {
-        Err(err) => plaid::print_debug_string(&format!("put_item error: {err}")),
+        Err(err) => {
+            plaid::print_debug_string(&format!("put_item error: {err}"));
+            return Err(1); // TODO: proper error code
+        }
         Ok(output) => {
             let json = serde_json::to_string(&output).unwrap();
             plaid::print_debug_string(&format!("put_item output: {json}"));
+            if json != json!({"attributes":null}) {
+                plaid::print_debug_string(&format!("error: put_item output_json mismatch: {json}"));
+                return Err(1); // TODO: proper error code
+            }
         }
     };
+
+    // Query
+    let input = QueryInput {
+        table_name: table_name.clone(),
+        key_condition_expression: String::from("#pk = :val"),
+        expression_attribute_names: Some(HashMap::from([("#pk".to_string(), "pk".to_string())])),
+        expression_attribute_values: Some(HashMap::from([(
+            ":val".to_string(),
+            Value::String(String::from("124")),
+        )])),
+        index_name: None,
+    };
+    let output = dynamodb::query(input).unwrap();
+    let output_json = to_value(&output).unwrap();
+    if output_json
+        != json!({"items":[{"age":33,"binaries":["ZGF0YTE=","ZGF0YTI="],"binary_field":{"_binary":"YmluYXJ5X2RhdGE="},"is_active":true,"metadata":{"city":"New York","country":"USA"},"name":"Jane Doe","null_field":null,"pk":"124","ratings":[3.8,4.5,5],"scores":[88,92,95],"tags":["aws","dev","rust"],"timestamp":"124"}]})
+    {
+        plaid::print_debug_string(&format!("error: query output_json mismatch: {output_json}"));
+        return Err(1); // TODO: proper error code
+    }
+
+    // Delete Item
+    let input = DeleteItemInput {
+        table_name,
+        key: HashMap::from([
+            (String::from("pk"), Value::String(String::from("124"))),
+            (
+                String::from("timestamp"),
+                Value::String(String::from("124")),
+            ),
+        ]),
+        return_values: Some(String::from("ALL_OLD")),
+        condition_expression: None,
+        expression_attribute_names: None,
+        expression_attribute_values: None,
+    };
+
+    let output = dynamodb::delete_item(input).unwrap();
+    let output_json = to_value(&output).unwrap();
+    if output_json
+        != json!({"attributes":{"age":33,"binaries":["ZGF0YTE=","ZGF0YTI="],"binary_field":{"_binary":"YmluYXJ5X2RhdGE="},"is_active":true,"metadata":{"city":"New York","country":"USA"},"name":"Jane Doe","null_field":null,"pk":"124","ratings":[3.8,4.5,5],"scores":[88,92,95],"tags":["aws","dev","rust"],"timestamp":"124"}})
+    {
+        plaid::print_debug_string(&format!(
+            "error: delete_item output_json mismatch: {output_json}"
+        ));
+        return Err(1); // TODO: proper error code
+    }
 
     Ok(())
 }
