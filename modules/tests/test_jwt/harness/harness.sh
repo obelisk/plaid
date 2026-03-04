a9df5db616ba10860c152ca3dddc3285eb22d650
Erik Burton <erikburt@users.noreply.github.com>
2026-03-03 18:18:07 -0800

    Add RSA to JWT API and Generic Enforced Claims (#269)
    * feat: support rs256 in jwt signing
    
    * feat: enforced_claims for JWT issuing
    
    * chore: add testing keys to apis/loading tomls
    
    * feat: test_jwt actually validate jwts
    
    * fix: jwt testing
    
    * chore: update expected response for test_jwt
    
    * chore: upgrade rustc
    
    * fix: make key_type mandatory
    
    * feat: custom error for enforced_claims
    
    * fix: integration.sh for macos
    
    * test: ugprade ci to rust 1.93.1
    
    * test: upgrade to wasmer 6.1
    
    * fix: test_jwt remove JWT validation
    
    I added JWT validation to the tests, however jsonwebtoken dependency
    expects a JS runtime when compiled to WASM. This causes errors when
    trying to call it from an LLVM/cranelift env.
    
    * test: use simple-jwt instead of jsonwebtoken
    
    * revert: dont validate jwts
    
    * fix: increase rsa key size
    
    * fix: JwtParams.aud handling
    
    * feat: improve jwt testing script

--- modules/tests/test_jwt/harness/harness.sh
+++ modules/tests/test_jwt/harness/harness.sh
@@ -20,7 +20,31 @@ sleep 2
 
 kill $RH_PID 2>&1 > /dev/null
 
-echo -e "OK\nOK\nOK\nOK\nOK\nOK\nOK\nOK" > expected.txt
+expected="OK - simple request (es256)
+OK - simple request (rs256)
+OK - add a field which is allowlisted (es256)
+OK - add aud which is an enforced claim but doesn't match enforced value (rs256)
+OK - add aud which is an enforced claim but matches enforced value (rs256)
+OK - add a field which is an enforced claim, and is not allowlisted (rs256)
+OK - add a field which is NOT allowlisted (es256)
+OK - add a header which is allowlisted (es256)
+OK - add a header which is NOT allowlisted (es256)
+OK - use a key ID that we are not allowed to use (es256)
+OK - use a key ID that does not exist (es256)
+OK - make a request without exp (es256)"
+echo -e "$expected" > expected.txt
+
+if [ "$DEBUG" = "true" ]; then
+  echo "Expected"
+  echo "--------"
+  cat expected.txt
+  echo ""
+  echo "Received"
+  echo "--------"
+  cat $FILE
+  echo ""
+fi
+
 diff expected.txt $FILE
 RESULT=$?
 
