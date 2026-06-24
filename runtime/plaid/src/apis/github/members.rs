13bcd008215a8829427ba5803a38a1f4c3c4a62b
Michele Minelli <michele.minelli@smartcontract.com>
2026-06-24 19:31:15 +0200

    Copilot review comments

--- runtime/plaid/src/apis/github/members.rs
+++ runtime/plaid/src/apis/github/members.rs
@@ -1,6 +1,5 @@
 use plaid_stl::github::{
-    CheckOrgMembershipParams, ConsumedLicensesResponse, GitHubUser, GithubApiWrapper,
-    RemoveOutsideCollaboratorParams,
+    CheckOrgMembershipParams, GitHubUser, GithubApiWrapper, RemoveOutsideCollaboratorParams,
 };
 
 use super::Github;
@@ -148,52 +147,5 @@ impl Github {
             Ok((_, Err(e))) => Err(e),
             Err(e) => Err(e),
         }
-    }
-
-    /// Get the number of consumed licenses for an enterprise
-    /// ## Arguments
-    /// * `client_id` - Selects which configured GitHub client to use (supports multiple clients).
-    /// * `enterprise` - The enterprise name
-    pub async fn get_enterprise_license_status(
-        &self,
-        params: &str,
-        module: Arc<PlaidModule>,
-    ) -> Result<String, ApiError> {
-        let request: GithubApiWrapper<String> =
-            serde_json::from_str(params).map_err(|_| ApiError::BadRequest)?;
-
-        let enterprise = self.validate_org(&request.params)?;
-
-        info!("Getting license status for enterprise [{enterprise}] on behalf of [{module}]");
-
-        // We don't really care about pagination because we are discarding information about users and
-        // only extracting the total number of consumed licenses, which is returned in the top-level JSON object.
-        let address = format!("/enterprises/{enterprise}/consumed-licenses");
-
-        // Instead of returning the full body to the rule, we parse it and only return info about total number of seats.
-        // This is a bit awkward in the sense that we are deserializing and then reserializing, but it allows us to
-        // avoid returning a lot of information that the rule doesn't need.
-        let body = match self
-            .make_generic_get_request(&request.client_id, address, module)
-            .await
-        {
-            Ok((status, Ok(body))) => {
-                if status == 200 {
-                    body
-                } else {
-                    return Err(ApiError::GitHubError(GitHubError::UnexpectedStatusCode(
-                        status,
-                    )));
-                }
-            }
-            Ok((_, Err(e))) => return Err(e),
-            Err(e) => return Err(e),
-        };
-
-        let consumed_licenses: ConsumedLicensesResponse = serde_json::from_str(&body)
-            .map_err(|_| ApiError::GitHubError(GitHubError::BadResponse))?;
-
-        Ok(serde_json::to_string(&consumed_licenses)
-            .map_err(|_| ApiError::GitHubError(GitHubError::BadResponse))?)
     }
 }
